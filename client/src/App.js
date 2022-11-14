6dca8e52aedbe12a8a0db9eb0c8837a32150a15d
Paulo Guedes <paulo.bkcoder@outlook.com>
2022-11-14 21:59:26 +0000

    Setup MERN; Connect DB; Get info from random API

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React from 'react';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const App = () => {
-    return (
-        <h1>App</h1>
-    )
+	const url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/covid-19-qppza/service/REST-API/incoming_webhook/metadata';
+	const [countries, setCountries] = useState([]);
+
+	useEffect(() => {
+		axios.get(url).then(res => {
+			setCountries(res.data.countries);
+		})
+	}, [])
+
+	return <div className="App">
+		<h1>List of Countries</h1>
+		<div>
+			<ul>
+				{countries.map(c => <li key={c}>{c}</li>)}
+			</ul>
+		</div>
+	</div>
 };
 
 export default App;
