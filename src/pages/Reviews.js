dab9a850895c7d4ab27690204477929e055647ea
Paulo Guedes <paulofguedes@outlook.pt>
2024-07-02 20:59:42 +0100

    dotenv; Search reviews by artist, Last.fm API

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -1,7 +1,60 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 function Reviews() {
+    const apiKey = process.env.REACT_APP_LASTFM_API_KEY;
+    const [artists, setArtists] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    function handleChange(e) {
+        const artistName = e.target.value;
+        setSearchTerm(artistName);
+                
+        if (artistName.trim() === '') {
+            setArtists([]);
+            return;
+        }
+
+        axios.get('http://ws.audioscrobbler.com/2.0/', {
+            params: {
+                method: 'artist.search',
+                artist: artistName,
+                api_key: apiKey,
+                format: 'json'
+            }
+        }).then(response => {
+            let artistResults = response.data.results?.artistmatches?.artist || [];
+            artistResults = artistResults.filter(item => {
+                return item.mbid !== '';
+            });
+
+            setArtists(Array.isArray(artistResults) ? artistResults : [artistResults]);
+        }).catch(error => {
+            console.error(error);
+            setArtists([]);
+        });
+    }
+
+
     return (
-        <div>
-            <h1>This is the REVIEWS page</h1>
+        <div className="p-5">
+            <input
+                placeholder="Search Artist"
+                className="w-full p-2 border rounded outline-none"
+                value={searchTerm}
+                onChange={handleChange}
+                autoFocus
+            />
+
+            <ul>
+                {artists.length === 0 ? (
+                    <div></div>
+                ) : (
+                    artists.map((artist, index) => (
+                        <li key={index}>{artist.name}</li>
+                    ))
+                )}
+            </ul>
         </div>
     );
 }
